c4afb8a884ad0f05d759a8e1e9b847c28fe38a0c
Wang Hao <haow1990@gmail.com>
2014-08-10 13:38:48 +0800

    leetcode-oj, Linked List Cycle

---
+++ leetcode-oj/linked-list-cycle.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
+
+int main()
+{
+    ListNode n1(1), n2(2), n3(3), n4(4), n5(5);
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+    //n5.next = &n2;
+
+    Solution s;
+    cout << s.hasCycle(NULL) << endl;
+    return 0;
+}
