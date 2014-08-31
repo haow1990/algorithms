fd647febb12c21f98ac0a8d54084483a60a51e34
Wang Hao <haow1990@gmail.com>
2014-09-01 00:31:24 +0800

    leetcode-oj, Wildcard Matching

--- leetcode-oj/wildcard-matching.cc
+++ leetcode-oj/wildcard-matching.cc
@@ -10,15 +10,22 @@ struct pattern {
     string str;
     int min;
     int max;
-    int got;
+    int minRight;
+    int maxRight;
 
     pattern(char cc)
-        : isPlain(true), str(1, cc), min(1), got(0)
+        : isPlain(true), str(1, cc), min(1), max(1), minRight(1), maxRight(1)
     {}
 
     pattern(int minval, int maxval)
-        : isPlain(false), str(), min(minval), max(maxval), got(0)
+        : isPlain(false), str(), min(minval), max(maxval), minRight(minval), maxRight(maxval)
     {}
+};
+
+enum status {
+    NONE,
+    YES,
+    NO
 };
 
 class Solution {
@@ -51,86 +58,135 @@ public:
                     ptn.push_back(pattern(*p));
                 } else {
                     ptn.back().str.push_back(*p);
+                    ptn.back().min += 1;
+                    ptn.back().max += 1;
                 }
             }
             
             ++p;
         }
+
+        if (ptn.empty()) {
+            return;
+        }
+
+        ptn.back().minRight = ptn.back().min;
+        ptn.back().maxRight = ptn.back().max;
+        for (int i = ptn.size() - 2; i >= 0; --i) {
+            ptn[i].minRight = ptn[i].min + ptn[i+1].minRight;
+            if (ptn[i].max == INT_MAX || ptn[i+1].maxRight == INT_MAX) {
+                ptn[i].maxRight = INT_MAX;
+            } else {
+                ptn[i].maxRight = ptn[i].max + ptn[i+1].maxRight;
+            }
+        }
     }
 
-    bool doMatch(const char *s, vector<pattern> &ptn, size_t pos)
+    bool doMatch(const string &str, size_t istr, vector<pattern> &ptn, size_t pos, vector<vector<int> > &array)
     {
-        cout << pos << endl;
+        const size_t ai = istr;
+        const size_t aj = pos;
+        if (array[ai][aj] != NONE) {
+            return array[ai][aj] == YES ? true : false;
+        }
         pattern &cp = ptn[pos];
         // source string comes to the end
-        if (*s == '\0') {
+        if (istr == str.size()) {
             // no pattern left, got end
             if (pos >= ptn.size()) {
+                array[ai][aj] = YES;
                 return true;
             }
             // not last pattern yet
             if (pos + 1 < ptn.size()) {
+                array[ai][aj] = NO;
                 return false;
             }
             // last pattern
-            return cp.isPlain == false
-                && cp.got >= cp.min;
+            bool result = cp.isPlain == false && cp.min == 0;
+            array[ai][aj] = result ? YES : NO;
+            return result;
         }
 
         // pattern comes to the end
         if (pos >= ptn.size()) {
+            array[ai][aj] = NO;
             return false;
         }
 
         // match current
         if (cp.isPlain) {
             // plain character must exactly match
-            size_t istr = 0;
-            while (*s && istr < cp.str.size() && *s == cp.str[istr]) {
-                ++istr;
-                ++s;
+            size_t ipstr = 0;
+            if (str.size() - istr < cp.str.size()) {
+                array[ai][aj] = NO;
+                return false;
             }
-            if (istr != cp.str.size()) {
+            while (ipstr < cp.str.size() && str[istr] == cp.str[ipstr]) {
+                ++ipstr;
+                ++istr;
+            }
+            if (ipstr != cp.str.size()) {
+                array[ai][aj] = NO;
                 return false;
             } else {
-                return doMatch(s, ptn, pos+1);
+                bool result = doMatch(str, istr, ptn, pos+1, array);
+                array[ai][aj] = result ? YES : NO;
+                return result;
             }
         } else {
             // glob, eat min characters first
-            while (*s && cp.got < cp.min) {
-                ++s;
-                ++cp.got;
-            }
-            // still hungry but source ends
-            if (cp.got < cp.min) {
+            if (str.size() - istr < cp.minRight) {
+                array[ai][aj] = NO;
                 return false;
             }
+            // last pattern
             if (pos + 1 == ptn.size()) {
-                return strlen(s) + cp.got <= cp.max;
+                bool result = str.size() - istr <= cp.max;
+                array[ai][aj] = result ? YES : NO;
+                return result;
             }
-            char expected = ptn[pos+1].str[0];
-            while (*s != '\0' && *s != expected) {
-                ++s;
+            if (str.size() - istr > cp.maxRight) {
+                array[ai][aj] = NO;
+                return false;
             }
-            // next pattern must be a plain, so find it first
-            do {
-                if (doMatch(s, ptn, pos + 1) == true) {
+            int start = istr + cp.min;
+            if (ptn[pos+1].maxRight != INT_MAX
+                    && start < str.size() - ptn[pos+1].maxRight) {
+                start = str.size() - ptn[pos+1].maxRight;
+            }
+            int end = str.size() - ptn[pos+1].minRight;
+            if (cp.max != INT_MAX && istr + cp.max < end) {
+                end = istr + cp.max;
+            }
+            if (str.size() - 1 < end) {
+                end = str.size() - 1;
+            }
+            int i = end;
+            const string &target = ptn[pos+1].str;
+            while ( i >= start) {
+                int j = 0;
+                while (j < target.size() && target[j] == str[i+j]) {
+                    ++j;
+                }
+                if (j >= target.size() && doMatch(str, i + target.size(), ptn, pos + 2, array)) {
+                    array[ai][aj] = YES;
                     return true;
                 }
-                if (*s == '\0') {
-                    break;
-                }
-                ++s;
-                ++cp.got;
-            } while (cp.got <= cp.max);
+                --i;
+            }
+            array[ai][aj] = NO;
             return false;
         }
+        return false;
     }
 
     bool isMatch(const char *s, const char *p) {
         vector<pattern> ptn;
         compile(p, ptn);
-        return doMatch(s, ptn, 0);
+        string str(s);
+        vector<vector<int> > array(str.size() + 1, vector<int>(ptn.size() + 1, NONE));
+        return doMatch(str, 0, ptn, 0, array);
     }
 };
 
